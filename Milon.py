626978a005adae2a39fd0fcf626badb9416069f6
Smr milon <108234691+SMRmilon01@users.noreply.github.com>
2022-07-04 11:42:00 +0600

    Update Milon.py

--- Milon.py
+++ Milon.py
@@ -60,9 +60,9 @@ def logo():
     print("\033[94m│     \033[92m ▌▌ ▌▗▚ ▐ ▌ ▖▌ ▌▌ ▌▌▐ ▌▌ ▌▛▀ ▌  \033[94m   │".center(columns+15))
     print("\033[94m│     \033[92m ▘▝▀ ▘ ▘▀▘▝▀ ▀▀ ▝▀ ▘▝ ▘▀▀ ▝▀▘▘  \033[94m   │".center(columns+15))
     print("\033[94m│                              \033[94m          │".center(columns+9))
-    print("\033[94m│ \033[95mAuthor : smrMilon                     \033[94m│".center(columns+15))
+    print("\033[94m│ \033[95mAuthor : ToxicNoob                     \033[94m│".center(columns+15))
     print("│ \033[95mTool   : Unlimited SMS Bomber          \033[94m│".center(columns+9))
-    print("│ \033[95mGitHub : https://github.com/SMRmilon01 \033[94m│".center(columns+9))
+    print("│ \033[95mGitHub : https://github.com/Toxic-Noob \033[94m│".center(columns+9))
     print("│ \033[95mCoder  : HunterSl4d3              \033[37mV3.0 \033[94m│".center(columns+15))
     print("\033[94m└────────────────────────────────────────┘".center(columns+5))
 
